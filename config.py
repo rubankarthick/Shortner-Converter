53fa16eb6941c73eceb9383ab448b70a33069387
RUBAN KARTHICK V <124701104+rubankarthick@users.noreply.github.com>
2023-08-15 20:20:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
+API_ID = int(os.getenv("API_ID", "25632035"))
+API_HASH = os.environ.get("API_HASH", "896d8f9929d3e00d2dae14646329fe3b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6280177829:AAHm5K6Xk6mtQHy7KSMr1MoWt1IkU3LdrCA")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Ruban Short")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://ruban9820:karthi9820@shortner.cx4lgyv.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "6094386527")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(6094386527)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001721952363")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "re_offcial") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
-LINK_BYPASS = "False"
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://telegra.ph/file/0988488e0d6fcf7bb3fd3.jpg') # image when someone hit /start # image when someone hit /start
+LINK_BYPASS = "True"
